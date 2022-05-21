4580d139748f40f3d481454f227077909904fe7a
re-wei <rd-wei@wei>
2022-05-21 15:12:12 +0800

    新增遊戲方法

--- card-matching/script.js
+++ card-matching/script.js
@@ -1,5 +1,3 @@
-// 25m
-// 音頻
 class AudioController {
     constructor() {
         this.bgMusic = new Audio('Assets/Audio/creepy.mp3')
@@ -8,7 +6,7 @@ class AudioController {
         this.victorySound = new Audio('Assets/Audio/victory.wav')
         this.gameOverSound = new Audio('Assets/Audio/gameOver.wav')
         this.bgMusic.volume = 0.5
-        this.bgMusic = true
+        this.bgMusic.loop = true
     }
     startMusic() {
         this.bgMusic.play()
@@ -25,7 +23,7 @@ class AudioController {
     }
     victory() {
         this.stopMusic()
-        this.victory.play()
+        this.victorySound.play()
     }
     gameOver() {
         this.stopMusic()
@@ -48,18 +46,88 @@ class MixOrMatch {
         this.timeRemaining = this.totalTime
         this.matchedCards = []
         this.busy = true
+        setTimeout(() => {
+            this.audioController.startMusic()
+            this.shuffleCards()
+            this.countdown = this.startCountDown()
+            this.busy = false
+        }, 500)
+        this.hideCards()
+        this.countdown = this.timeRemaining
+        this.ticker.innerText = this.totalClicks
+    }
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible')
+            card.classList.remove('matched')
+        })
     }
     flipCard(card) {
         if (this.canFlipCard(card)) {
             this.audioController.flip()
             this.totalClicks++
             this.ticker.innerText = this.totalClicks
+            card.classList.add('visible')
+
+            if (this.cardToCheck)
+                this.checkForCardMatch(card)
+            else
+                this.cardToCheck = card
+        }
+    }
+    checkForCardMatch(card) {
+        if (this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck)
+        else
+            this.cardMisMatch(card, this.cardToCheck)
+        this.cardToCheck = null
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1)
+        this.matchedCards.push(card2)
+        card1.classList.add('matched')
+        card2.classList.add('matched')
+        this.audioController.match()
+        if (this.matchedCards.length === this.cardsArray.length) this.victory()
+    }
+    cardMisMatch(card1, card2) {
+        this.busy = true
+        setTimeout(() => {
+            card1.classList.remove('visible')
+            card2.classList.remove('visible')
+            this.busy = false
+        }, 1000)
+    }
+    getCardType(card) {
+        return card.getElementsByClassName('card-value')[0].src
+    }
+    startCountDown() {
+        return setInterval(() => {
+            this.timeRemaining--
+            this.timer.innerText = this.timeRemaining
+            if (this.timeRemaining === 0) this.gameOver()
+        }, 1000)
+    }
+    gameOver() {
+        clearInterval(this.countDown)
+        this.audioController.gameOver()
+        document.getElementById('game-over-text').classList.add('visible')
+    }
+    victory() {
+        clearInterval(this.countDown);
+        this.audioController.victory()
+        document.getElementById('victory-text').classList.add('visible')
+    }
+    shuffleCards() {
+        for (let i = this.cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i + 1))
+            this.cardsArray[randIndex].style.order = i
+            this.cardsArray[i].style.order = randIndex;
         }
     }
 
     canFlipCard(card) {
-        return true
-        // return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
+        return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
     }
 }
 // const { createReadStream } = require("fs")
